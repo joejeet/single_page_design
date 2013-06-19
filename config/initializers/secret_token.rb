0411a6043f84787e2171eaff9b04e1a15320d3f1
joejeet <joe@enternships.com>
2013-06-19 19:16:13 +0100

    Initialize account

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountManagement::Application.config.secret_token = '9446513b3dff1674a1abda6e9164dee5fed6064c091f9b2b12881c58bf6a462e5b47c834333d52c58549841ddbacddb86c96146cb3f6920d866e47fd2cd56e9a'
